5668458037afb2cf83742eac6301c116f8c5347f
akithap <akithaperera6@gmail.com>
2026-04-21 01:50:09 +0530

    Redirect entire Storage path to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel Serverless Requests to the Laravel public index
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// CRITICAL FOR VERCEL: Force Laravel to use the ephemeral Writable /tmp disk for EVERYTHING it tries to cache or write to internally.
+$app->useStoragePath('/tmp');
+
+// Handle the request!
+$app->handleRequest(Request::capture());
